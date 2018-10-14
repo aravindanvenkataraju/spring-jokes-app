9a7b07f7a9a5e234bd84f143df78dfc8577bfd66
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2018-10-15 00:11:54 +1100

    final-version

--- src/main/java/com/aravindan/jokes/controller/JokesController.java
+++ src/main/java/com/aravindan/jokes/controller/JokesController.java
@@ -1,8 +1,27 @@
 package com.aravindan.jokes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aravindan.jokes.services.JokesService;
 
 @Controller
 public class JokesController {
-
+	
+	private JokesService jokesService;
+	
+	@Autowired
+	public JokesController(JokesService jokesService) {
+		super();
+		this.jokesService = jokesService;
+	}
+	@RequestMapping({"/", ""})
+	public String showQuote(Model model) {
+		model.addAttribute("joke", jokesService.getQuotes());
+		return "chucknorris";
+	}
+	
+	
 }
